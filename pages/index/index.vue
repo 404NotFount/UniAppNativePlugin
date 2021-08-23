85cde2ef60e6e022231220e27f6a2d059f5b65b0
aixiaodong <43229097@qq.com>
2021-08-23 09:55:20 +0800

    uniAPP测试原生插件包

---
+++ pages/index/index.vue
@@ -0,0 +1,73 @@
+<template>
+    <view class="container">
+        姓名<input v-model="name" placeholder="点此编辑姓名" >
+        <br>
+        年龄<input v-model="age" placeholder="点此编辑年龄" >
+        <br>
+        <button type="default" @click="test">提交</button>
+		<button type="default" @click="test1">跳转</button>
+        <view>{{name}}</view>
+        <view>{{age}}</view>
+        <view>{{ result }}</view>
+		<view>{{ result1 }}</view>
+    </view>
+</template>
+
+<script>
+    var testModule = uni.requireNativePlugin("Hik-Video-Module");
+    export default {
+        data() {
+            return {
+                name:"",
+                age:"",
+                result:"暂未提交",
+				result1:"暂未提交"
+            }
+        },
+        methods: {
+            test(){
+                testModule.testText({
+                    'name':this.name,
+                    'age':this.age,
+                },
+                (ret) =>{
+                    this.result="[提交反馈]"+ret.code;
+                })
+            },
+			test1(){
+				testModule.gotoPreviewActivity({
+					'previewUri':this.name,
+					'cameraCode':'129e41335b704ee5b0f24aa6bfba64b6'
+				})
+			}
+        }
+    }
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
